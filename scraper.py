e8e59085621c6bc9b675eb4c197bbafdd7f20925
Leonard Yulianus <leonard.yulianus@gmail.com>
2017-07-14 04:23:10 -0400

    Rearrange code

--- scraper.py
+++ scraper.py
@@ -4,31 +4,30 @@ from lxml import html
 
 base = 'https://boardgamegeek.com'
 url = '/browse/boardgame'
-games = {}
 last_page = False
 
-while not last_page:
-    # Get and page content
-    page = requests.get(base+url).text
-    doc = html.fromstring(page)
-
-    # Get list of games
-    game_list = doc.cssselect('td.collection_objectname a')
-    for game in game_list:
-        id = game.attrib['href'].split('/')[-2]
-        title = game.text_content()
-        games[id] = title
-
-    # Check next page link
-    link = doc.cssselect('#collection+p a')[-2]
-    url = link.attrib['href']
-
-    if "Next" not in link.text_content():
-        last_page = True
-
-# Export games to csv
 with open('games.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['id', 'title'])
-    for game in games.items():
-        writer.writerow(game)
+
+    while not last_page:
+        # Print status
+        print('Scraping {}'.format(base+url))
+
+        # Get and page content
+        page = requests.get(base+url).text
+        doc = html.fromstring(page)
+
+        # Get list of games and write them to the csv file
+        game_list = doc.cssselect('td.collection_objectname a')
+        for game in game_list:
+            id = game.attrib['href'].split('/')[-2]
+            title = game.text_content()
+            writer.writerow([id, title])
+
+        # Check next page link
+        link = doc.cssselect('#collection+p a')[-2]
+        url = link.attrib['href']
+
+        if "Next" not in link.text_content():
+            last_page = True
