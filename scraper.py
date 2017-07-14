99487fb82321edbc667f3b13f3d75816d42d1402
Leonard Yulianus <leonard.yulianus@gmail.com>
2017-07-14 04:14:36 -0400

    Add export function

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from lxml import html
 
 base = 'https://boardgamegeek.com'
@@ -15,8 +16,8 @@ while not last_page:
     game_list = doc.cssselect('td.collection_objectname a')
     for game in game_list:
         id = game.attrib['href'].split('/')[-2]
-        name = game.text_content()
-        games[id] = name
+        title = game.text_content()
+        games[id] = title
 
     # Check next page link
     link = doc.cssselect('#collection+p a')[-2]
@@ -24,3 +25,10 @@ while not last_page:
 
     if "Next" not in link.text_content():
         last_page = True
+
+# Export games to csv
+with open('games.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['id', 'title'])
+    for game in games.items():
+        writer.writerow(game)
