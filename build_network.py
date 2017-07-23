ffce3a8f72689fb86fcbf0846d4fbe84997e6f58
Leonard Yulianus <leonard.yulianus@gmail.com>
2017-07-23 01:44:26 -0400

    Only use game that has more than 500 votes

--- build_network.py
+++ build_network.py
@@ -1,34 +1,49 @@
-import networkx as nx
 import csv
+import networkx as nx
+from itertools import combinations
 
 G = nx.Graph()
+groups = {}
 
 with open('games.csv', 'r') as infile:
     reader = csv.DictReader(infile)
 
     for game in reader:
-        # Print status
-        print('Processing {}'.format(game['title']))
-
+        id = int(game['id'])
+        title = game['title']
+        try:
+            num_voters = int(game['num_voters'])
+        except:
+            num_voters = 0
         traits = set(game['categories'].split(',') + game['mechanisms'].split(','))
         traits.discard('')
 
+        # Skip game that has no traits or has less than 500 voters
+        if len(traits) == 0 or num_voters < 500:
+            continue
+
+        # Print status
+        print('Processing {}'.format(title))
+
         # Add game into the graph
-        G.add_node(game['id'], title=game['title'], traits=traits)
+        G.add_node(id, title=title, ntrait=len(traits))
 
-        # Link game with existing games based on shared traits
-        for existing_game in G.nodes_iter():
-            if existing_game == game['id']:
-                continue
+        # Group similar games based on traits
+        for trait in iter(traits):
+            try:
+                groups[trait].append(id)
+            except:
+                groups[trait] = [id]
 
-            for trait in iter(traits):
-                if trait not in G.node[existing_game]['traits']:
-                    continue
+for trait, games in iter(groups.items()):
+    # Print status
+    print('Processing {}'.format(trait))
 
-                if G.has_edge(game['id'], existing_game):
-                    G[game['id']][existing_game]['weight'] += 1
-                else:
-                    G.add_edge(game['id'], existing_game, weight=1)
+    for source, target in combinations(games, 2):
+        if G.has_edge(source, target):
+            G[source][target]['weight'] += 1
+        else:
+            G.add_edge(source, target, weight=1)
 
-    # save the network
-    nx.write_gexf(G, 'boardgame.gexf')
+# Save the network
+nx.write_gml(G, 'boardgame.gml')
